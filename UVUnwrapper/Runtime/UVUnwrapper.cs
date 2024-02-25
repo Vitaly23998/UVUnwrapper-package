1df4e63a8912936bf7b8c1e80f412eb9bc6bda90
Viktor Rakhimov <viktor@cyclingbear.studio>
2024-02-25 13:57:26 +0300

    Init

---
+++ UVUnwrapper/Runtime/UVUnwrapper.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEditor
+{
+[ExecuteInEditMode]
+	public class AutoUVUnwrapping : MonoBehaviour
+	{
+		[MenuItem("Utils/Generate Lightmap UVs for Selection")]
+		public static void GenerateLightmapUVs()
+		{
+			GameObject[] selectedObjects = Selection.gameObjects;
+			foreach (GameObject selectedObject in selectedObjects)
+			{
+				MeshFilter meshFilter = selectedObject.GetComponent<MeshFilter>();
+				if (meshFilter != null)
+				{
+					Unwrapping.GenerateSecondaryUVSet(meshFilter.sharedMesh);
+				}
+			}
+		}
+	}
+}
